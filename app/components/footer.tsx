9d65be228083c5199f5b28a5066aae1b95a903b4
Vinicius Mattei <viniciuspmattei@gmail.com>
2024-08-12 12:26:55 +0200

    Changed footer and Blog page

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -54,7 +54,7 @@ export default function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        Vinícius Mattei - © {new Date().getFullYear()} MIT Licensed
       </p>
     </footer>
   )
