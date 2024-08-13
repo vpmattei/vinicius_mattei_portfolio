8c746c26c329f162e92f05ef76350b1a4bb298e3
Vinicius Mattei <viniciuspmattei@gmail.com>
2024-08-13 14:04:29 +0200

    Adding hebald forest logo and centering items in games page

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -17,7 +17,7 @@ function ArrowIcon() {
 
 export default function Footer() {
   return (
-    <footer className="mb-16">
+    <footer className="mb-16 ml-8 mr-8">
       <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
