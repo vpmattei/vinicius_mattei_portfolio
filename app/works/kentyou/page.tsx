2fa7af744dc711a249c7fbffa68b6fa29bbf9319
Vinicius Mattei <viniciuspmattei@gmail.com>
2024-09-24 15:00:12 +0200

    Added Kentyou logo to its page

--- app/works/kentyou/page.tsx
+++ app/works/kentyou/page.tsx
@@ -20,32 +20,40 @@ const images = [
 const Page: React.FC = () => {
   return (
     <section className="prose ml-8 mr-8 flex flex-col items-center gap-16">
-      <h2 className="mb-16">Kentyou</h2>
-      <br></br>
-      <a
-        className="mb-8 flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-        rel="noopener noreferrer"
-        target="_blank"
-        href="https://www.figma.com/proto/pBTikcvwgShCHDAPnP5tvN/kentyou-ui-library__core--client?node-id=3414-13558&starting-point-node-id=646%3A17244&t=BXHG4WbblYkKL9N9-1"
-      >
-        <h3>Kentyou UI Prototype</h3>
-        <p className="ml-2">
-          <ArrowIcon size={18} />
+      <div className="w-[100%] flex flex-col items-center">
+        <img
+          src="https://pbs.twimg.com/profile_images/1328342235038048263/gHLzQIHN_400x400.png"
+          alt="Kentyou Logo"
+          style={{ width: 250 }}
+        />
+        <h2 className="mb-16">Kentyou</h2>
+        <a
+          className="mb-8 flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.figma.com/proto/pBTikcvwgShCHDAPnP5tvN/kentyou-ui-library__core--client?node-id=3414-13558&starting-point-node-id=646%3A17244&t=BXHG4WbblYkKL9N9-1"
+        >
+          <h3>Kentyou UI Prototype</h3>
+          <p className="ml-2">
+            <ArrowIcon size={18} />
+          </p>
+        </a>
+        <p className="text-xl indent-8 text-justify">
+          During my internship at Kentyou, a startup where I worked as a UI/UX
+          Designer and Developer, I redesigned their dashboard overview. One key
+          feature I introduced was an edit toggle button that allows users to
+          easily switch between viewing and editing their dashboard.
+          Additionally, I designed a profile settings page that enables users to
+          update their profile name, change their password, and adjust various
+          other settings. All designs were created using Figma.
         </p>
-      </a>
-      <p className="text-xl indent-8 text-justify">
-        During my internship at Kentyou, a startup where I worked as a UI/UX
-        Designer and Developer, I redesigned their dashboard overview. One key
-        feature I introduced was an edit toggle button that allows users to
-        easily switch between viewing and editing their dashboard. Additionally,
-        I designed a profile settings page that enables users to update their
-        profile name, change their password, and adjust various other settings.
-        All designs were created using Figma.
-      </p>
+      </div>
       <FullScreenGallery images={images} />
 
       <div className="previous-next-work-buttons">
-        <a className="previous-work" href="/works">All works</a>
+        <a className="previous-work" href="/works">
+          All works
+        </a>
         <a className="next-work" href="/works/tangled-tales">
           Tangled Tales
         </a>
