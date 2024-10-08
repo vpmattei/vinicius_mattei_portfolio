4ff1546a075e44a0b40d2726ee11f82b638a6be7
Vinicius Mattei <viniciuspmattei@gmail.com>
2024-10-08 14:53:04 +0200

    Redone home page by adding all works

--- app/page.tsx
+++ app/page.tsx
@@ -32,19 +32,58 @@ export default function Page() {
         </p>
       </div>
 
-      <div className="m-8 flex flex-col items-center">
-        <CustomImage
-          src="https://i.ibb.co/19SxrGF/paravoile.png"
-          alt="Hebald Forest"
-          link="/works/hebald-forest"
-          maxWidth="40rem"
-        />
-        <a href="/works/hebald-forest">
-          <div className="w-full flex flex-col items-center">
+      <div className="m-8 md:m-16 lg:m-48 flex flex-col items-center gap-32">
+        <div className="self-start">
+          <CustomImage
+            src="https://i.ibb.co/19SxrGF/paravoile.png"
+            alt="Hebald Forest"
+            link="/works/hebald-forest"
+            maxWidth="40rem"
+          />
+          <a href="/works/hebald-forest">
             <h2>Hebald Forest</h2>
             <p>Open world adventure game – Unreal Engine 5</p>
-          </div>
-        </a>
+          </a>
+        </div>
+
+        <div className="self-end">
+          <CustomImage
+            src="https://ludhic.fr/wp-content/uploads/2023/01/anamnesis02.webp"
+            alt="Anamnesis"
+            link="/works/anamnesis"
+            maxWidth="40rem"
+          />
+          <a href="/works/anamnesis">
+            <h2>Anamnesis</h2>
+            <p>Narrative VR Game – Unity</p>
+          </a>
+        </div>
+
+        <div className="self-start">
+          <CustomImage
+            src="https://i.pinimg.com/564x/77/e2/c2/77e2c23bd4a54c852e15c146079e4b7c.jpg"
+            alt="Tangled Tales"
+            link="/works/tangled-tales"
+            maxWidth="40rem"
+          />
+          <a href="/works/tangled-tales">
+            <h2>Tangled Tales</h2>
+            <p>2D Platformer – Unity</p>
+          </a>
+        </div>
+
+        <div className="self-end">
+          <CustomImage
+            src="https://i.ibb.co/GPvMSqy/Captura-de-Tela-2024-09-13-s-12-00-21.png"
+            alt="Kentyou"
+            link="/works/kentyou"
+            maxWidth="40rem"
+          />
+          <a href="/works/kentyou">
+            <h2>Kentyou Website Design</h2>
+            <p>Web Design of Kentyou's website using Figma</p>
+          </a>
+        </div>
       </div>
     </section>
   );
